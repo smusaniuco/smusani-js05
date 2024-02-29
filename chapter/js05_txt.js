3958b4154873f70cecb73fe5169c7df151c2539b
SHYAM <SMUSANI@UCO.EDU>
2024-02-29 10:48:18 -0600

    Init Commit

---
+++ chapter/js05_txt.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Chapter Case
+
+      Application to generate a slide show
+      Author: 
+      Date:   
+
+      Filename: js05.js
+*/
